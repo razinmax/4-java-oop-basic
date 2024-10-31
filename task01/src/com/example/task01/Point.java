620589d761819678b009f6593a9a0f7713878b53
Max Razin <razinmax02@gmail.com>
2024-10-31 20:49:48 +0500

    Task01 complete

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,28 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip(){
+        int temp = x;
+        x = -y;
+        y = -temp;
+    }
+
+    double distance(Point point){
+        int deltaX = this.x - point.x;
+        int deltaY = this.y - point.y;
+        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
