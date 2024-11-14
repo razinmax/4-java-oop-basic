7aa0827c3d1f21bda7a3b331adf0865f8b8c2591
Max Razin <razinmax02@gmail.com>
2024-11-14 21:13:48 +0500

    Task02 complete

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -3,6 +3,6 @@ package com.example.task02;
 public class Task02Main {
     public static void main(String[] args) {
         TimeSpan time = new TimeSpan(54, 160, 81);
-        System.out.println(time.toString());
+        System.out.println(time);
     }
 }
